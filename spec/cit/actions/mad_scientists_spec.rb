d0adea6a6cd7dde5b1e914767aa408c861f80190
Alexander Ilchukov <alex.ilchukov@gmail.com>
2018-09-02 08:09:23 +0300

    #5 Add action of destroying of record of a mad scientist

--- spec/cit/actions/mad_scientists_spec.rb
+++ spec/cit/actions/mad_scientists_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe CIT::Actions::MadScientists do
   describe 'the module' do
     subject { described_class }
 
-    it { is_expected.to respond_to(:create) }
+    it { is_expected.to respond_to(:create, :destroy) }
   end
 
   describe '.create' do
@@ -28,6 +28,40 @@ RSpec.describe CIT::Actions::MadScientists do
 
     it 'should create new record of mad scientists' do
       expect { subject }.to change { CIT::Models::MadScientist.count }.by(1)
+    end
+  end
+
+  describe '.destroy' do
+    subject { described_class.destroy(params, rest) }
+
+    let(:params) { data }
+    let(:data) { { id: id } }
+    let!(:id) { mad_scientist.id }
+    let(:mad_scientist) { create(:mad_scientist) }
+    let(:rest) { nil }
+
+    it_should_behave_like 'an action parameters receiver', wrong_structure: {}
+
+    it 'should destroy the record of mad scientist with provided identifier' do
+      expect { subject }.to change { CIT::Models::MadScientist.count }.by(-1)
+    end
+
+    context 'when the record can\'t be found by provided identifier' do
+      let(:id) { create(:uuid) }
+
+      it 'should raise Sequel::NoMatchingRow' do
+        expect { subject }.to raise_error(Sequel::NoMatchingRow)
+      end
+    end
+
+    context 'when there is a record of a doomsday machine of the scientist' do
+      let!(:doomsday_machine) { create(:doomsday_machine, traits) }
+      let(:traits) { { mad_scientist_id: id } }
+
+      it 'should raise Sequel::ForeignKeyConstraintViolation' do
+        expect { subject }
+          .to raise_error(Sequel::ForeignKeyConstraintViolation)
+      end
     end
   end
 end
